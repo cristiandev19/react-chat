ea27555470510bc2922fc3fce5728ad2d7ec273c
cristiandev <cristiansotomayor.dev@gmail.com>
2021-03-28 23:33:21 -0500

    agregando un context

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,35 @@ import {
   Switch,
   Route,
 } from 'react-router-dom';
+import { useEffect, useReducer } from 'react';
 
 import Home from './pages/Home';
 import Chat from './pages/Chat/Chat';
+import AuthContext from './auth/AuthContext';
+import authReducer from './auth/AuthReducer';
+
+const init = () => JSON.parse(localStorage.getItem('userChat')) || { logged: false };
 
 function App() {
+  const [user, dispatch] = useReducer.Provider(authReducer, {}, init);
+
+  useEffect(() => {
+    localStorage.setItem('userChat', JSON.stringify(user));
+  }, [user]);
+
   return (
-    <Router>
-      <Switch>
-        <Route path="/chat/:idChat">
-          <Chat />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
+    <AuthContext.Provider value={{ user, dispatch }}>
+      <Router>
+        <Switch>
+          <Route path="/chat/:idChat">
+            <Chat />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </AuthContext.Provider>
   );
 }
 
